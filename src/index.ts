87c70da08384940dde9bf6394bd4765bc629b256
jimao <jimao@jimaodeMacBook-Pro.local>
2022-05-12 18:16:06 +0800

    提交

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,33 @@
 import axios from 'axios';
+import { JSDOM } from 'jsdom';
 
 console.log('你好，世界');
 
+async function getAllFileUrl(page_url: string, page_path: string) {
+  const result: string[] = [];
+  let next_marker = undefined;
+  do {
+    const rsp = await axios.get(page_url, {
+      params: {
+        delimiter: '/',
+        prefix: page_path,
+        marker: next_marker,
+      },
+    });
+    if (rsp.status === 200) {
+      const xmlText = rsp.data as string;
+      const document = new JSDOM(xmlText).window.document;
+      const keys = Array.from(document.querySelectorAll('ListBucketResult > Contents > Key'));
+      result.push(...keys.map((key) => key.innerHTML.trim()));
+      next_marker = document.querySelector('ListBucketResult > NextMarker')?.innerHTML;
+    }
+  } while (next_marker);
+  return result;
+}
+
 async function main() {
-  const rsp = await axios.get('https://s3-ap-northeast-1.amazonaws.com/data.binance.vision', {
-    params: {
-      delimiter: '/',
-      prefix: 'data/futures/um/daily/klines/BTCUSDT/15m/',
-      marker: undefined,
-    },
-  });
-  if (rsp.status === 200) {
-    const xmlText = rsp.data as string;
-    console.log(xmlText.length);
-  }
+  const a = await getAllFileUrl('https://s3-ap-northeast-1.amazonaws.com/data.binance.vision', 'data/futures/um/daily/klines/BTCUSDT/15m/');
+  console.log(a.length);
 }
 
 main();
