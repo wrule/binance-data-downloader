55c594b44eb0d1328abcea645823ab172b510057
jimao <jimao@jimaodeMacBook-Pro.local>
2022-05-12 18:45:24 +0800

    单个文件下载

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import axios from 'axios';
 import path from 'path';
 import { JSDOM } from 'jsdom';
+import fs from 'fs';
+import moment from 'moment';
+
+export
+function log(name: string, text = '') {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss:SSS')} ${name}]${text ? `: ${text}` : '...'}`);
+}
 
 async function getAllFileUrl(
   api: string,
@@ -21,20 +28,34 @@ async function getAllFileUrl(
       const xmlText = rsp.data as string;
       const document = new JSDOM(xmlText).window.document;
       const keys = Array.from(document.querySelectorAll('ListBucketResult > Contents > Key'));
-      result.push(...keys.map((key) => path.join(file_base_url, key.innerHTML.trim())));
+      result.push(...keys.map((key) => file_base_url + key.innerHTML.trim()));
       next_marker = document.querySelector('ListBucketResult > NextMarker')?.innerHTML;
     }
   } while (next_marker);
   return result;
 }
 
+async function downloadFile(
+  file_url: string,
+  dir_path: string,
+) {
+  try {
+    log('下载文件', file_url);
+    const rsp = await axios.get(file_url, { responseType: 'stream' });
+    rsp.data.pipe(fs.createWriteStream(path.join(dir_path, path.basename(file_url))));
+  } catch (e) {
+    log('下载出错，重试下载', file_url);
+    await downloadFile(file_url, dir_path);
+  }
+}
+
 async function main() {
   const a = await getAllFileUrl(
     'https://s3-ap-northeast-1.amazonaws.com/data.binance.vision',
     'data/futures/um/daily/klines/BTCUSDT/15m/',
-    'https://data.binance.vision',
+    'https://data.binance.vision/',
   );
-  console.log(a[0]);
+  await downloadFile(a[0], 'src');
 }
 
 main();
