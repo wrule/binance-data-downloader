24fd7d5c98ad4fd4fd4f249cae8c3ed9c7ecca09
jimao <jimao@jimaodeMacBook-Pro.local>
2022-05-12 17:49:56 +0800

    发出请求

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
+import axios from 'axios';
 
 console.log('你好，世界');
+
+async function main() {
+  const rsp = await axios.get('https://s3-ap-northeast-1.amazonaws.com/data.binance.vision', {
+    params: {
+      delimiter: '/',
+      prefix: 'data/futures/um/daily/klines/BTCUSDT/15m/',
+    },
+  });
+  if (rsp.status === 200) {
+    const xmlText = rsp.data as string;
+    console.log(xmlText.length);
+  }
+}
+
+main();
